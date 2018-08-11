d15a2e40286543a6cd3bf1138e87972a6fe70eb4
Nick Davis <nickdavisa@gmail.com>
2018-08-11 02:30:02 -0700

    Created background

--- js/game.js
+++ js/game.js
@@ -11,3 +11,18 @@ let config = {
 
 // create game with config
 let game = new Phaser.Game(config);
+
+// loads all assets for game
+game_scene.preload = function() {
+    // load background asset
+    this.load.image('background', 'assets/background.png');
+};
+
+// executes once after all assets loaded
+game_scene.create = function() {
+    // create background
+    let bg = this.add.sprite(0, 0, "background");
+
+    // change origin
+    bg.setOrigin(0, 0);
+};
