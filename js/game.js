cb3a66200925c68e75c9fe1776e4c0f001a3dc71
Nick Davis <nickdavisa@gmail.com>
2018-08-12 01:35:35 -0700

    Added spaceship

--- js/game.js
+++ js/game.js
@@ -14,15 +14,16 @@ let game = new Phaser.Game(config);
 
 // loads all assets for game
 game_scene.preload = function() {
-    // load background asset
     this.load.image('background', 'assets/background.png');
+    this.load.image('spaceship', 'assets/spaceship.png');
 };
 
 // executes once after all assets loaded
 game_scene.create = function() {
     // create background
     let bg = this.add.sprite(0, 0, "background");
-
+    let ship = this.add.sprite(0, 0, "spaceship");
     // change origin
     bg.setOrigin(0, 0);
+    ship.setOrigin(-9, -5);
 };
